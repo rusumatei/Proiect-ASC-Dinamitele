b866513491827103cbd035119ccfea7d746e1d85
suciu199 <suciumaria940@gmail.com>
2026-01-08 22:07:52 +0200

    Partea de cod-Maria

--- sursa.asm
+++ sursa.asm
@@ -1,111 +1,143 @@
 DATA SEGMENT
-    word_c           dw 0
-    temp_byte        db 0
+    msg_sorted       db 13,10,'2. Sirul sortat (Descrescator): $'
+    msg_max_bits     db 13,10,'3. Pozitia octetului cu cei mai multi biti de 1 (>3): $'
+    msg_no_max       db 13,10,'3. Nu exista niciun octet cu mai mult de 3 biti de 1.',13,10,'$'
+    msg_rotated      db 13,10,'4. Sirul dupa rotiri (Hex si Bin):',13,10,'$'
     
-    msg_word_c       db 13, 10, '1. Cuvantul C calculat (Hex): $'
-    msg_word_c_bin   db 13, 10, '   Cuvantul C calculat (Bin): $'
+    space            db ' $'
+    arrow            db ' -> $'
+    
+    max_bits_val     db 0
+    max_bits_idx     db 0
 DATA ENDS
 
 CODE SEGMENT
     ASSUME CS:CODE, DS:DATA
 
-CALCULATE_C PROC NEAR
-    xor ax, ax
-    xor cx, cx
+SORT_DESCENDING PROC NEAR
     mov cl, array_count
-    lea si, byte_array
-SUM_LOOP:
-    xor bx, bx
-    mov bl, [si]
-    add ax, bx
-    inc si
-    loop SUM_LOOP
-    mov byte ptr [word_c + 1], al
-
-    lea si, byte_array
-    mov bl, [si]
-    shr bl, 4
-
-    xor di, di
-    mov al, array_count
-    cbw
-    mov di, ax
-    dec di
-    mov bh, byte_array[di]
-    and bh, 0Fh
-
-    xor bl, bh
-    and bl, 0Fh
-    mov temp_byte, bl
-
-    xor dx, dx
-    mov cl, array_count
-    lea si, byte_array
-OR_LOOP:
-    mov al, [si]
-    and al, 00111100b
-    shr al, 2
-    or  dl, al
-    inc si
-    loop OR_LOOP
-
-    and dl, 0Fh
-    shl dl, 4
-
-    mov al, temp_byte
-    or  al, dl
-    mov byte ptr [word_c], al
-    ret
-CALCULATE_C ENDP
-
-APPLY_ROTATIONS PROC NEAR
-    lea si, byte_array
-    mov ch, 0
-    mov cl, array_count
-
-ROT_LOOP:
-    push cx
-    mov al, [si]
-
-    xor bh, bh
-    test al, 1
-    jz  CHECK_B1
-    inc bh
-CHECK_B1:
-    test al, 2
-    jz  DO_ROT
-    inc bh
-
-DO_ROT:
-    call ROTATE_BYTE_LEFT
-    mov [si], al
-
-    push ax
-    mov bh, al
-    call PRINT_HEX_BYTE
-    lea dx, arrow
-    call PRINT_STRING
-    pop ax
-
-    mov bh, al
-    call PRINT_BIN_BYTE
-    lea dx, newline
-    call PRINT_STRING
-
-    inc si
-    pop cx
     dec cl
-    jnz ROT_LOOP
+OUTER:
+    push cx
+    lea si, byte_array
+    mov ch, array_count
+    dec ch
+INNER:
+    mov al, [si]
+    mov ah, [si+1]
+    cmp al, ah
+    jae NO_SWAP
+    mov [si], ah
+    mov [si+1], al
+NO_SWAP:
+    inc si
+    dec ch
+    cmp ch, 0
+    jg INNER
+    pop cx
+    loop OUTER
     ret
-APPLY_ROTATIONS ENDP
+SORT_DESCENDING ENDP
 
-ROTATE_BYTE_LEFT PROC NEAR
-    mov cl, bh
-    cmp cl, 0
-    je ROT_DONE
-    rol al, cl
-ROT_DONE:
+FIND_MAX_BITS_POS PROC NEAR
+    lea si, byte_array
+    mov cl, array_count
+    xor di, di
+    mov max_bits_val, 0
+    mov max_bits_idx, 0
+
+SCAN_LOOP:
+    mov al, [si]
+    push cx
+    mov cx, 8
+    xor bl, bl
+    mov dl, al
+COUNT_BITS:
+    shl dl, 1
+    adc bl, 0
+    loop COUNT_BITS
+    pop cx
+
+    cmp bl, max_bits_val
+    jle NEXT_VAL
+    mov max_bits_val, bl
+    mov ax, di
+    mov max_bits_idx, al
+
+NEXT_VAL:
+    inc si
+    inc di
+    dec cl
+    jnz SCAN_LOOP
     ret
-ROTATE_BYTE_LEFT ENDP
+FIND_MAX_BITS_POS ENDP
+
+PRINT_ARRAY PROC NEAR
+    lea si, byte_array
+    mov cl, array_count
+    xor ch, ch
+PR_LOOP:
+    mov bh, [si]
+    call PRINT_HEX_BYTE
+    lea dx, space
+    call PRINT_STRING
+    inc si
+    loop PR_LOOP
+    ret
+PRINT_ARRAY ENDP
+
+PRINT_HEX_BYTE PROC NEAR
+    push ax
+    push bx
+    push cx
+    push dx
+
+    mov dl, bh
+    shr dl, 4
+    call PRINT_NIBBLE
+    mov dl, bh
+    and dl, 0Fh
+    call PRINT_NIBBLE
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+PRINT_HEX_BYTE ENDP
+
+PRINT_NIBBLE PROC NEAR
+    cmp dl, 9
+    jle P_DIGIT
+    add dl, 7
+P_DIGIT:
+    add dl, '0'
+    mov ah, 02h
+    int 21h
+    ret
+PRINT_NIBBLE ENDP
+
+PRINT_BIN_BYTE PROC NEAR
+    push cx
+    push dx
+    mov cx, 8
+P_BIN_LOOP:
+    shl bh, 1
+    jc P_ONE
+    mov dl, '0'
+    jmp P_OUT
+P_ONE:
+    mov dl, '1'
+P_OUT:
+    mov ah, 02h
+    int 21h
+    loop P_BIN_LOOP
+    mov dl, ' '
+    mov ah, 02h
+    int 21h
+    pop dx
+    pop cx
+    ret
+PRINT_BIN_BYTE ENDP
 
 CODE ENDS
